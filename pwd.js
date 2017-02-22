5ebcb75b0f7087d206150a8a81f4846d0e076a9d
Fabrizio Rizzi <fabrizio@fabriziorizzi.it>
2017-02-22 15:49:18 +0100

    Aggiunto funzionalità che se non c'è carattere speciale ne aggiunge uno alla fine al posto dell'ultimo carattere

--- pwd.js
+++ pwd.js
@@ -1,33 +1,39 @@
 var $ = function (id) {
     return document.getElementById(id);
 }
-var generateClick = function () {
+var generateClick = () => {
+	let set;
+	let num = parseInt($("num").value);
+	let pwd = "";
+	let value2 = false;
 	if (document.getElementsByName('set')[0].checked) {
-		var set ="abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+		set ="abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	} else {
-		var set ="abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!£$%&-_()=?"
+		set ="abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"=?@"
 	}
-	var num = parseInt($("num").value);
-	var pwd = "";
 	if (isNaN(num) || num <= 0 || num > 20){
 		$("error").firstChild.nodeValue = "Length must be a number between 1 and 20";
 	} else {
-		var value2 = true;
+		value2 = true;
 	}
 	if (value2) {
 		for (i = 1; i <= num; i++) {
 			pwd += set.charAt(Math.floor(Math.random() * set.length));
 		}
 	}
+	if (!pwd.match(/!|\"|\=|\?|\@/g) && !document.getElementsByName('set')[0].checked) {
+		pwd = pwd.slice(0, -1);
+		pwd += "!\"=?@".charAt(Math.floor(Math.random() * "!\"=?@".length))
+	}
 	$("pwd").value = pwd;
 }
-var clear = function() {
+var clear = () => {
 			$("num").value = "8";
 			$("pwd").value = "";
 			$("error").firstChild.nodeValue = "*";
 			$("num").select();
 }
-window.onload = function () {
+window.onload = () => {
 	$("clear").onclick = clear;
 	$("generate").onclick = generateClick;
 	$("num").select();
